d104431a5fd87e0452a5d875f6a6d08cde64ae13
João Pedro Maciel <joomaciiel2@outlook.com>
2020-09-08 18:16:24 -0300

    simplificando estrutura inicial

--- resources/lang/pt_BR/passwords.php
+++ resources/lang/pt_BR/passwords.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
-    | Password Reset Language Lines
+    | Password Reminder Language Lines
     |--------------------------------------------------------------------------
     |
     | The following language lines are the default lines which match reasons
@@ -13,10 +12,9 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
-
+    'reset'     => 'Sua senha foi redefinida!',
+    'sent'      => 'Enviamos um link para redefinir a sua senha por e-mail.',
+    'throttled' => 'Por favor espere antes de tentar novamente.',
+    'token'     => 'Esse código de redefinição de senha é inválido.',
+    'user'      => 'Não conseguimos encontrar nenhum usuário com o endereço de e-mail informado.',
 ];
