e860b1abb84dfd8cc2bd938270756a97cc35abd9
João Pedro Maciel <joomaciiel2@outlook.com>
2020-09-08 21:26:31 -0300

    header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,26 +8,33 @@
     <title>JAOFLIX</title>
 
     <!-- Bootstrap -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+
+    {{-- font awesome --}}
+    <script src="https://kit.fontawesome.com/270632f1ff.js" crossorigin="anonymous"></script>
 
   </head>
   <body>
 
-  <div class="container">
+  {{-- header --}}
+  @include('templates.header')
 
-      {{-- header --}}
-      @include('templates.header')
+    <div class="container">
 
       {{-- conteúdo --}}
       @yield('conteudo')
 
-  </div>
+    </div>
 
 
-    <!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- BOOTSTRAP (frontend framework) -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
-    <!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
+
+    <!-- JAO'S SCRIPTS -->
 
   </body>
 </html>
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,1 +1,28 @@
-HEADER
+<header>
+    <ul class="nav">
+        <a href="" ><img class="img-logo" src="{{ asset('images/jaoflix.jpg') }}" alt="logotipo"></a>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Inicio</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Séries</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Filmes</a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="#">Infantil</a>
+        </li>
+
+        <li>
+            <i class="fas fa-search"></i>
+            <input type="text" class="form-control" placeholder="Buscar">
+        </li>
+
+    </ul>
+</header>
+
