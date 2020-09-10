0c2ba2ccddcd7568dc220f748022d29f06866f61
João Pedro Maciel <joomaciiel2@outlook.com>
2020-09-10 11:33:28 -0300

    footer

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,92 @@
+<footer class="footer-padrao page-footer font-small indigo">
+
+    <div class="separador"></div>
+
+    <!-- Footer Links -->
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="https://joomaciiel.github.io/github_pages/pages/sobremim.html">Sobre mim</a>
+          </h6>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="https://joomaciiel.github.io/github_pages/pages/portfolio.html">Portfolio</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="https://joomaciiel.github.io/github_pages/">Contato</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+      <hr class="rgba-white-light" style="margin: 0 15%;">
+
+      <!-- Grid row-->
+      <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+        <!-- Grid column -->
+        <div class="texto-footer col-md-8 col-12 mt-5">
+          <p style="line-height: 1.5rem">Este é um projeto fictício, desenvolvido por <a href="https://github.com/joomaciiel">João Pedro Maciel</a>,
+        apenas para finalidade de estudo e prática, não existe nenhum filme ou série para assistir aqui, apenas
+        exemplifico como seria um projeto de site de streaming, com um usuário administrador que consegue fazer
+        o CRUD da aplicação.</p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+      <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+      <!-- Grid row-->
+      <div class="icones-footer text-center row pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-12">
+
+          <div class="mb-5 flex-center">
+
+            <!-- Facebook -->
+            <a href="https://www.facebook.com/JooMaciiel" class="fb-ic">
+              <i class="fab fa-facebook-f fa-lg white-text mr-4"></i>
+            </a>
+            <!--Linkedin -->
+            <a href="https://www.linkedin.com/in/joomaciiel/" class="li-ic">
+              <i class="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
+            </a>
+            <!--Instagram-->
+            <a href="https://www.instagram.com/joomaciiel/" class="ins-ic">
+              <i class="fab fa-instagram fa-lg white-text mr-4"> </i>
+            </a>
+
+          </div>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="https://github.com/joomaciiel"> joomaciiel</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
